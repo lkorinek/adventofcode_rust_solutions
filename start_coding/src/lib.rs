23058b8b877143406171775125b1e83c46ae1488
korinlu <korinlu1@fel.cvut.cz>
2024-12-08 17:30:15 +0900

    Added parser of arguments (file, input, test, day, browser) and browser options

--- start_coding/src/lib.rs
+++ start_coding/src/lib.rs
@@ -1,6 +1,38 @@
 use chrono::{Datelike, Local};
+use clap::ValueEnum;
 use reqwest::blocking::Client;
-use rookie::brave;
+use rookie::{brave, chrome, firefox};
+use std::fmt;
+use std::fmt::{Display, Formatter};
+use std::process::exit;
+#[cfg(target_os = "macos")]
+#[derive(ValueEnum, Debug, Clone)]
+pub enum Browsers {
+    Brave,
+    Chrome,
+    Firefox,
+    Safari,
+}
+
+#[cfg(not(target_os = "macos"))]
+#[derive(ValueEnum, Debug, Clone)]
+pub enum Browsers {
+    Brave,
+    Chrome,
+    Firefox,
+}
+
+impl Display for Browsers {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Browsers::Brave => write!(f, "Brave"),
+            Browsers::Chrome => write!(f, "Chrome"),
+            Browsers::Firefox => write!(f, "Firefox"),
+            #[cfg(target_os = "macos")]
+            Browsers::Safari => write!(f, "Safari"),
+        }
+    }
+}
 
 const ADVENT_OF_CODE_WEBSITE: &str = "adventofcode.com";
 
@@ -19,10 +51,25 @@ impl AutoInputScraper {
         }
     }
 
-    pub fn get_result(&self, day: Option<u32>) -> String {
+    pub fn get_result(&self, day: Option<u32>, browser: Browsers) -> String {
         let domains = vec![String::from(ADVENT_OF_CODE_WEBSITE)];
-        let cookies = brave(Some(domains)).expect("Cookies not found in Brave.");
-        let cookie = cookies.get(0).expect("No cookie found.");
+        let cookies = match browser {
+            #[cfg(target_os = "macos")]
+            Browsers::Safari => safari(Some(domains)),
+            Browsers::Brave => brave(Some(domains)),
+            Browsers::Chrome => chrome(Some(domains)),
+            Browsers::Firefox => firefox(Some(domains)),
+        };
+
+        let cookies = cookies.unwrap_or_else(|_err| {
+            println!("AOC Cookies not found in {browser} browser.");
+            exit(0); // Exit if cookies are not found
+        });
+
+        let cookie = cookies.get(0).unwrap_or_else(|| {
+            println!("AOC Cookies not found in {browser} browser.");
+            exit(1); // Exit with status 1 if no cookie is found
+        });
 
         let day_to_get = if let Some(day_changed) = day {
             day_changed
--- start_coding/src/main.rs
+++ start_coding/src/main.rs
@@ -1,46 +1,122 @@
 use chrono::{Datelike, Local};
-use start_coding::AutoInputScraper;
+use clap::{arg, Parser};
+use start_coding::{AutoInputScraper, Browsers};
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 use std::process::{exit, Command};
-use std::{env, fs};
+use std::{fs, io};
 
+/// A program that creates a Cargo package and downloads the current day's Advent of Code puzzle input
+#[derive(Parser, Debug)]
+#[command(version = "1.1", about)]
+struct Args {
+    /// Name of the file
+    #[arg(short, long, default_value_t = String::from("input"))]
+    file_name: String,
+
+    /// Download just the input
+    #[arg(long, default_value_t = false)]
+    no_package: bool,
+
+    /// Specifies the test mode (day is prefixed with 'test_')
+    #[arg(short, long, default_value_t = false)]
+    test: bool,
+
+    /// Specifies the puzzle day
+    #[arg(short, long)]
+    day: Option<u32>,
+
+    /// Specifies the browser to use
+    #[arg(long = "browser", short = 'b', value_name = "BROWSER", value_enum, default_value_t = Browsers::Brave)]
+    browser: Browsers,
+}
 const PACKAGE_NAME: &str = "day";
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
     let now = Local::now();
-    let day: u32 = match args.len() {
-        1 => now.day(),
-        _ => {
-            let arg = &args[1]; // First argument is the day of the month
-            arg.parse().expect("Invalid number of the day.")
-        }
+    let day = if let Some(day) = args.day {
+        day
+    } else {
+        now.day()
     };
 
-    let new_package = format!("{PACKAGE_NAME}{day}");
-    let status = Command::new("cargo")
-        .arg("new")
-        .arg(&new_package)
-        .status()
-        .expect("Failed to execute command");
-
-    if status.success() {
-        println!("Successfully created package '{}'", new_package);
-    } else {
-        eprintln!("Failed to create package '{}'", new_package);
+    let new_package = format!(
+        "{}{PACKAGE_NAME}{day}",
+        if args.test { "test_" } else { "" }
+    );
+    if new_package.is_empty() {
+        eprintln!("Error: Package name cannot be empty.");
         exit(1);
+    }
+    if !args.no_package {
+        if !Path::new(&new_package).exists() {
+            let status = Command::new("cargo")
+                .arg("new")
+                .arg(&new_package)
+                .status()
+                .expect("Failed to execute command");
+            if status.success() {
+                println!("Successfully created package '{}'", new_package);
+            } else {
+                eprintln!("Failed to create package '{}'", new_package);
+                exit(1);
+            }
+        } else if should_continue(&format!(
+            "The directory '{new_package}' already exists and will be removed."
+        )) {
+            if new_package.is_empty() {
+                eprintln!("Error: Package name cannot be empty.");
+                exit(1);
+            }
+            fs::remove_dir_all(&new_package).expect("Failed to remove existing directory");
+            let status = Command::new("cargo")
+                .arg("init")
+                .arg(&new_package)
+                .status()
+                .expect("Failed to execute command");
+
+            if status.success() {
+                println!("Successfully initialized package '{}'", new_package);
+            } else {
+                eprintln!("Failed to initialize package '{}'", new_package);
+                exit(1);
+            }
+        } else {
+            exit(0);
+        }
     }
 
     let scraper = AutoInputScraper::new();
-    let alg_input = scraper.get_result(Some(day));
+    let alg_input = scraper.get_result(Some(day), args.browser);
 
-    let dir = format!("{new_package}/target/debug");
+    let dir = format!("{new_package}/");
     fs::create_dir_all(&dir).unwrap();
-    let file_path = Path::new(&dir).join("input.txt");
+    let file_path = Path::new(&dir).join(args.file_name);
     let mut file = File::create(file_path).unwrap();
 
     file.write_all(alg_input.as_bytes()).unwrap();
+}
+
+fn should_continue(extra: &str) -> bool {
+    print!(
+        "{}Do you want to continue? (y/N): ",
+        if !extra.is_empty() {
+            format!("{extra}\n")
+        } else {
+            "".to_owned()
+        }
+    );
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let input = input.trim();
+
+    match input.to_lowercase().as_str() {
+        "y" | "Y" | "yes" | "Yes" => true,
+        _ => false,
+    }
 }
